392f4ac206f15c4863499a9b5fdb08c2c53f4273
Mateo Bustamante <mateo.bustamante9730@gmail.com>
2024-03-02 11:18:35 -0500

    editor de mobs con exportacion json

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,154 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { RefObject } from 'react';
 import './App.css';
+import MobTypeData, { MobTemplate, LootItems } from './data/MobTypes';
+import Items from './data/Items';
 
 function App() {
+
+  const MobId = React.useRef<HTMLSelectElement>(null);
+  const typeNewItem = React.useRef<HTMLSelectElement>(null);
+  const qtyNewItem = React.useRef<HTMLInputElement>(null);
+  const lootItem = React.useRef<HTMLLIElement>(null);
+  let MobObj: MobTemplate | undefined;
+  const [MobGlobalData, setMobGlobal] = React.useState<Array<MobTemplate | undefined>>(MobTypeData);
+  const [MobActual, setMobActual] = React.useState<MobTemplate | undefined>(MobObj);
+
+  function addValueInput (event: React.ChangeEventHandler<HTMLInputElement>) {
+    // let element = (event.target as HTMLInputElement).value
+
+  }
+
+  function exportData () {
+    const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+      JSON.stringify(MobGlobalData)
+    )}`;
+    const link = document.createElement("a");
+    link.href = jsonString;
+    link.download = "data.json";
+    link.click();
+  }
+
+  function handleMobId (event: React.ChangeEvent<any>) {
+    MobId.current!.value = event.target.value
+    setMobActual(MobGlobalData.find((mob) => mob?.id === MobId?.current?.value));
+  }
+  
+  function deleteLootMob (event : React.MouseEvent, id: string | undefined) : void {
+    event.stopPropagation();
+    function filterItem (actual: MobTemplate | undefined){
+      let newItems : Array<LootItems>;
+      if (actual != undefined) {
+        if (actual?.items) {
+          newItems = actual?.items.filter((item) => id !== item.itemId)
+          actual.items = newItems;
+          return actual;
+        }
+      }
+    }
+    setMobActual(filterItem(MobActual));
+
+  }
+
+  const deepMergeObj = (...objects : Array<any>) => {
+    const CopiasObj = objects.map(obj => JSON.parse(JSON.stringify(obj)))
+    const Reduce =  CopiasObj.reduce((merged, current) => ({...current, ...merged}), {})
+    return Reduce
+  }
+
+  function addLootMob () : void {
+    function pushItem (actual: MobTemplate | undefined) : MobTemplate | undefined {
+      const itemValue = typeNewItem.current?.value ?? '';
+      const qtyValue = parseInt(qtyNewItem.current?.value ?? '', 10)
+      let nuevoLoot: {
+        itemId: string | undefined;
+        quantity: number | undefined;
+      } = {
+        itemId: itemValue,
+        quantity: qtyValue
+      }
+      if (actual != undefined) {
+        if (!actual?.items) {
+          actual!.items = []
+        }
+        let lootFiltro = actual?.items!.filter((item) => item.itemId !== nuevoLoot.itemId)
+        lootFiltro.push(nuevoLoot)
+        actual.items = lootFiltro
+      }
+      return actual;
+    }
+    setMobActual(pushItem(MobActual));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <article className='Tarjeta'>
+      <header className='Tarjeta__header'>
+        <h4>Monster Editor</h4>
       </header>
-    </div>
+      <section className='Tarjeta__head'>
+        <figure className='Tarjeta__imagen'>
+          <img src="" alt="" />
+        </figure>
+        <select ref={MobId} onChange={handleMobId} className='Tarjeta__mobSelector' name="monster[id]" id="">
+          <option>Select</option>
+          {MobGlobalData.map((mob, key) => {
+            return (
+              <option key={key} value={mob?.id}>{mob?.name}</option>
+            );
+          })}
+        </select>
+      </section>
+      <section className='Tarjeta__form'>
+        <form className='Formulario -MobType' action="">
+          <label className='Formulario__campo -MobType' htmlFor="">
+            <span>Name</span>
+            <input id='monster-name' name='monster[name]' value={MobActual?.name ?? ''} type="text" />
+          </label>
+          <label className='Formulario__campo -MobType' htmlFor="">
+            <span>Description</span>
+            <textarea id='monster-description' value={MobActual?.description ?? ''} name="monster[description]" cols={22} rows={10}></textarea>
+          </label>
+          <label className='Formulario__campo -MobType' htmlFor="">
+            <span>Weight</span>
+            <input id='monster-weight' name='monster[weight]' type="number" />
+          </label>
+          <label className='Formulario__campo -MobType' htmlFor="">
+            <span>Hit points</span>
+            <input id='monster-hitPoints' name='monster[hitPoints]' type="number" />
+          </label>
+          <label className='Formulario__campo -MobType' htmlFor="">
+            <span>Attack</span>
+            <input id='monster-attack' name='monster[attack]' type="number" />
+          </label>
+          <ul className='Formulario__dropList -MobType' id='dropItemList'>
+            {
+              MobActual?.items?.map((item, key) => {
+                return (
+                  <li className='Lista__item -DropList' ref={lootItem} key={item.itemId}>
+                    <span>{`Drop ${item.quantity} ${item.itemId}`}</span>
+                    <button className='Boton -eliminar -DropList' type='button' onClick={e => deleteLootMob(e, item.itemId)}><b>X</b></button>
+                  </li>
+                );
+              })
+            }
+          </ul>
+          <fieldset className='Formulario__dropOption -MobType'>
+            <select ref={typeNewItem} name="monster[dropItem]" id="monster_dropItem">
+              <option>Select</option>
+              {Items.map((item, key) => {
+                return (
+                  <option key={key} value={item.name}>{item.name}</option>
+                );
+              })}
+            </select>
+            <input ref={qtyNewItem} className='' type="number" name='monster[dropQty]' id='monster_dropItemQty' />
+          </fieldset>
+        </form>
+      </section>
+      <footer className='Tarjeta__footer'>
+        <button type='button' onClick={addLootMob} className='Boton -addLoot'>Add Loot</button>
+      </footer>
+      <button className='Boton -exportar' type='button' onClick={exportData}>Exportar</button>
+    </article>
   );
 }
 
---
+++ src/data/MobTypes.ts
@@ -0,0 +1,194 @@
+//TODO: Make this a data file instead of a JS module
+
+import { StringLiteral } from "typescript";
+
+const MobTypes : Array<MobTemplate> = [
+	{
+		id: 'demon',
+		appearance: 'demon',
+		name: 'Gargoyle',
+		hp: 20,
+		damage: 3,
+		defense: 0,
+		corpse: 'demonCorpse',
+		weapon: 'boomerang',
+		speed: 6
+	},
+	{
+		id: 'avatar',
+		appearance: 'man1',
+		name: 'Avatar',
+		hp: 200,
+		damage: 15,
+		defense: 2,
+		speed: 5,
+		corpse: 'manCorpse',
+		portrait: 'portraitAvatar'
+	},
+	{
+		id: 'man1',
+		appearance: 'man1',
+		name: 'Man',
+		hp: 20,
+		damage: 3,
+		defense: 0,
+		speed: 5,
+		corpse: 'manCorpse'
+	},
+	{
+		id: 'iolo',
+		appearance: 'iolo',
+		name: 'Iolo',
+		hp: 20,
+		damage: 3,
+		defense: 0,
+		speed: 5,
+		corpse: 'manCorpse',
+		portrait: 'portraitIolo'
+	},
+	{
+		id: 'shamino',
+		appearance: 'man2',
+		name: 'Shamino',
+		hp: 200,
+		damage: 3,
+		defense: 0,
+		speed: 5,
+		corpse: 'manCorpse',
+		portrait: 'portraitShamino'
+	},
+
+	{
+		id: 'corvus',
+		appearance: 'man2',
+		name: 'Corvus',
+		hp: 200,
+		damage: 3,
+		defense: 0,
+		speed: 5,
+		corpse: 'manCorpse',
+		portrait: 'portraitShamino'
+	},
+
+	{
+		id: 'calista',
+		appearance: 'soldier',
+		name: 'Calista',
+		hp: 200,
+		damage: 3,
+		defense: 0,
+		speed: 5,
+		corpse: 'manCorpse',
+		portrait: 'portraitShamino'
+	},
+
+	{
+		id: 'rat',
+		appearance: 'rat',
+		name: 'Rat',
+		hp: 20,
+		damage: 3,
+		defense: 0,
+		speed: 3,
+		corpse: 'ratCorpse'
+	},
+	{
+		id: 'cow',
+		appearance: 'cow',
+		name: 'Cow',
+		hp: 50,
+		damage: 4,
+		defense: 0,
+		speed: 2,
+		corpse: 'cowCorpse',
+		useEffect: {
+			type: 'milk'
+		}
+	},
+	{
+		id: 'skeleton',
+		appearance: 'skeleton',
+		name: 'Skeleton',
+		hp: 20,
+		damage: 3,
+		defense: 0,
+		speed: 3,
+		corpse: 'bones'
+	},
+	{
+		id: 'swordSoldier',
+		appearance: 'soldier',
+		name: 'Soldier',
+		description: 'Just a guy named Joe.',
+		hp: 10,
+		damage: 5,
+		defense: 3,
+		speed: 2,
+		corpse: 'manCorpse',
+		intent: 'waitCommand',
+		alignment: 'enemy',
+		weapon: 'sword',
+		items: [
+			{ itemId: 'gold', quantity: 10 },
+			{ itemId: 'silver', quantity: 10 },
+			{ itemId: 'wood', quantity: 10 },
+			{ itemId: 'cheese', quantity: 10 }
+		]
+	},
+	{
+		id: 'xbowSoldier',
+		appearance: 'soldier',
+		name: 'Soldier',
+		description: 'Watch out! he\'s got a crossbow!.',
+		hp: 5,
+		damage: 3,
+		defense: 3,
+		speed: 2,
+		corpse: 'manCorpse',
+		intent: 'waitCommand',
+		alignment: 'enemy',
+		weapon: 'crossbow',
+		items: [
+			{ itemId: 'ironBolt', quantity: 10 }
+		]
+	},
+	{
+		id: 'asteroth',
+		appearance: 'asteroth',
+		name: 'Asteroth',
+		description: 'Lord of Empath Abbey.',
+		hp: 500,
+		damage: 5,
+		defense: 20,
+		speed: 2,
+		corpse: 'manCorpse',
+		intent: 'waitCommand',
+		alignment: 'enemy',
+		weapon: 'crossbow',
+		items: [
+			{ itemId: 'ironBolt', quantity: 10 }
+		]
+	}
+];
+
+export type LootItems = { itemId: string | undefined, quantity: number | undefined };
+type UseEffect = { type: string };
+export interface MobTemplate {
+		id: string;
+		appearance: string;
+		name: string;
+		description?: string;
+		hp: number;
+		damage: number;
+		defense: number;
+		speed: number;
+		corpse: string;
+		intent?: string;
+		alignment?: string;
+		weapon?: string;
+		items?: Array<LootItems>;
+		portrait?: string;
+		useEffect?: UseEffect;
+};
+
+export default MobTypes;
